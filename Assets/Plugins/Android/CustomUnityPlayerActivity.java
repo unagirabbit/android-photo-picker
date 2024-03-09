2544d2d57b47ebb3e7986b2f8fa28d0863906554
usagi <usagithebekt@gmail.com>
2024-03-09 10:59:36 +0900

    add: custom activity

---
+++ Assets/Plugins/Android/CustomUnityPlayerActivity.java
@@ -0,0 +1,10 @@
+package com.unagirabbit.photopicker;
+import com.unity3d.player.UnityPlayerActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class CustomUnityPlayerActivity extends UnityPlayerActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
